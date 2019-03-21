0f2989c0ad1ba6c1d06f9829c7318e18711d8735
shamsheer142089 <shamsheer142089@gmail.com>
2019-03-21 15:51:54 +0530

    Version 2 of hellor world

--- example-one/src/HelloGoWorld.go
+++ example-one/src/HelloGoWorld.go
@@ -1,7 +1,24 @@
-package src
+package main
 
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World ... Welcome to  go programming")
+	//go execution always starts with func main method ..
+	// This is the first golang examples wiht basic control flow
+	//It has sequential flow , loop and conditional flow
+	fmt.Println("Hello World ... Welcome to  go programming")//sequential
+	even()//sequential
+	exit()
+}
+
+func even(){
+	for  count:=0;count<=100;count++{//loop
+		if count%2==0{//conditional
+			fmt.Println(count)
+		}
+	}
+}
+
+func exit(){
+	fmt.Println("Leaving bbye~~~~~~~~~~~~")
 }
